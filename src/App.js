a5e49f138721679d7e03fd349d57dd98f3c97397
Nikita Sidukov <nsidukov@icloud.com>
2022-11-27 02:15:58 +0300

    prop2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
 import "./App.css";
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+  React.useEffect(() => {
+    tg.ready();
+  }, []);
+  const onClose = () => {
+    tg.close();
+  };
   return (
     <div className="App">
-      work<button>закрыть</button>
+      work<button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
